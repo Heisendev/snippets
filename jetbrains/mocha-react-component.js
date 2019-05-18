6fb2c020dc1249125106397e3df10fd92f9a6157
Jordi Dosne <jordi@dosne.net>
2019-05-18 17:22:53 +0200

    Update mocha-react-component.js

--- jetbrains/mocha-react-component.js
+++ jetbrains/mocha-react-component.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import expect from 'expect';
 import { shallow } from 'enzyme';
-import ${ComponentName} from './';
+import ${NAME} from './';
 
-describe('<${ComponentName} />', () => {
+describe('<${NAME} />', () => {
   function defaultProps() {
     return {};
   }
   it('should always render as a div', () => {
     const props = defaultProps();
-    const wrapper = shallow(<${ComponentName} {...props} />);
+    const wrapper = shallow(<${NAME} {...props} />);
     expect(wrapper.type()).to.equal('div');
   });
 });
